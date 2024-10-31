9101e6a5ae73da486bba17168fbc23482fa66ede
Zach Dodson <zdodson21@gmail.com>
2024-10-31 16:03:23 -0400

    getting started

--- src/CodeBlock.ts
+++ src/CodeBlock.ts
@@ -4,24 +4,47 @@ import { property } from 'lit/decorators.js';
 export class CodeBlock extends LitElement {
   static styles = css`
     :host {
-      display: block;
-      padding: 25px;
-      color: var(--code-block-text-color, #000);
+      display: inline-block;
+    }
+
+    .code-block-wrapper {
+      display: inline-flex;
+      justify-content: center;
+      align-items: center;
+      background-color: #1e2329;
+      color: white;
+      padding: 0px 12px;
+      border-style: solid;
+      border-color: white;
+      border-width: 1px;
+      border-radius: 8px;
+      font-size: 16px;
+      font-family: monospace;
+    }
+
+    :host([allow-copy]) .code-block-wrapper:hover, :host([allow-copy]) .code-block-wrapper:focus {
+      cursor: pointer;
+      background-color: #2e2e2e;
+      border-width: 2px;
     }
   `;
 
-  @property({ type: String }) header = 'Hey there';
+  @property({ type: Boolean, attribute: 'allow-copy', reflect: true }) allowCopy = false;
 
-  @property({ type: Number }) counter = 5;
+  @property({ type: Boolean, attribute: 'copy-alert', reflect: true }) copyAlert = false;
 
-  __increment() {
-    this.counter += 1;
-  }
+  // TODO make dependencyAlert property
+
+  // TODO make copyToClipboard() function
+  // __increment() {
+  //   this.counter += 1;
+  // }
 
   render() {
     return html`
-      <h2>${this.header} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      <div class="code-block-wrapper">
+        <p><slot></slot></p>
+      </div>
     `;
   }
 }
